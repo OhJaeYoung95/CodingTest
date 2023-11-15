837e31f39619c07b0f7ac7d7c229a50cdc81b6c0
OhJaeYoung <ostyoung@naver.com>
2023-11-15 23:45:21 +0900

    1115 - 페인팅 성공

--- C#/Painting_1115/Painting/Program.cs
+++ C#/Painting_1115/Painting/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+        public static int solution(int n, int m, int[] section)
+        {
+            int answer = 0;
+            int range = section[0] - 1;
+
+            for (int i = 0; i < section.Length; ++i)
+            {
+                if (section[i] > range)
+                {
+                    range = section[i] - 1 + m;
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int wallSize = 5;
+            int brushSize = 2;
+            int[] section = new int[] { 1, 4, 5};
+
+            Console.WriteLine(solution(wallSize, brushSize, section));
         }
     }
 }
