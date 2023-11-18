536713cce82566fb38bd93920e77dd42bcb327b0
OhJaeYoung <ostyoung@naver.com>
2023-11-18 21:05:40 +0900

    1118

---
+++ C#/DisappearingScaffolding/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DisappearingScaffolding
+{
+    internal class Program
+    {
+        public enum Position
+        {
+            Y,
+            X
+        }
+
+
+
+        public static int solution(int[,] board, int[] aloc, int[] bloc)
+        {
+            int answer = -1;
+            return answer;
+        }
+
+        public static bool IsMoveable(int[,] board, int[] player)
+        {
+            if(board[player[(int)Position.Y + 1], player[(int)Position.X + 1]] == 0)
+            {
+
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] board = { { 1, 1, 1},
+                              { 1, 1, 1},
+                              { 1, 1, 1} };
+            int[] aloc = { 1, 0 };
+            int[] bloc = { 1, 2 };
+
+            Console.WriteLine(solution(board, aloc, bloc));
+        }
+    }
+}
